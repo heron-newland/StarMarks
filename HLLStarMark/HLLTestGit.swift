3dd35422f9fa1bfa515614975228196864b292fc
bochb <bochb@bochbdeimac.local>
2017-11-02 15:21:58 +0800

    测试

---
+++ HLLStarMark/HLLTestGit.swift
@@ -0,0 +1,13 @@
+//
+//  HLLTestGit.swift
+//  HLLStarMark
+//
+//  Created by  bochb on 2017/11/2.
+//  Copyright © 2017年 com.heron. All rights reserved.
+//
+
+import UIKit
+
+class HLLTestGit: NSObject {
+
+}
